3d8b135e5fc36b66a214dba5e0ca6431560673b0
Weilory <54391313+Weilory@users.noreply.github.com>
2020-08-21 15:38:33 +1000

    Update password.php

--- pet/php/password.php
+++ pet/php/password.php
@@ -13,10 +13,13 @@ update user encrypted user password to databsae by Users::UpdatePassword
 
 if(isset($_POST['password'])){
 	require_once('../php/users.php');
+	require_once('../php/sess.php');
+	$session_handler = new Sess();
 	session_start();
 	$users_obj = new Users();
-	$users_obj->setId($_SESSION['id']);
+	$users_obj->setId($_SESSION['pass']['id']);
 	$users_obj->setPassword(md5($_POST['password']));
+	unset($_SESSION['pass']);
 	if($users_obj->updatePassword()){
 		// throw the redirect location to reset.js->ajax.done
 		require_once('../php/inverse.php');
